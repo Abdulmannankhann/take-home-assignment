472f423a18ba3b4f4372559c5f2207f2a6bbb43d
Abdul9.khan <Abdul9.khan@ril.com>
2023-10-25 00:24:05 +0530

    Solve Task 1.0.5

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	env:{
+		MONGODB_URI:"mongodb+srv://mukesh:12345@cluster0.hej3agm.mongodb.net/hirechain?retryWrites=true&w=majority"
+	},
   reactStrictMode: true,
 }
 
